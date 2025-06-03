c705be958fdae01a46afc4fe43d15bf66a8646a0
leehojindesu <leehojin02@naver.com>
2025-06-04 01:23:40 +0900

    레거시로 시간 구현(추후에 TextPro으로 변경 가능)

---
+++ Assets/Enemys/TimeUI.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeUI : MonoBehaviour
+{
+    public float Time;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
