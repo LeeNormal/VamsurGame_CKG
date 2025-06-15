0e3a24b4e7aa48ce6ac4492a8289becb6c69ed04
leehojindesu <leehojin02@naver.com>
2025-06-15 20:11:18 +0900

    시간 지나면 적들 원으로 플레이어 압박 코드

--- Assets/Enemys/EnemyScript/EnemySpawner.cs
+++ Assets/Enemys/EnemyScript/EnemySpawner.cs
@@ -1,3 +1,4 @@
+using Enemys.EnemyScript;
 using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
@@ -9,16 +10,27 @@ public class EnemySpawner : MonoBehaviour
     private bool endSpawned = false;
     
     private float timer = 0f;
+    private float wavetimer = 0f;
     private float endtimer = 0f;
+    
     private float interval = 2f;
-
+    private float waveInterval = 30f;
+    private int waveCounter = 20;
     private void Update()
     {
         timer += Time.deltaTime;
         endtimer += Time.deltaTime;
+        wavetimer += Time.deltaTime;
+        
         if (!(timer >= interval)) return;
         SpawnEnemy();
         timer = 0f;
+        if (wavetimer >= waveInterval)
+        {
+            SpawnCircleEnemies(waveCounter, 8f);
+            wavetimer = 0f;
+            waveCounter += 10;
+        }
     }
 
     private void SpawnEnemy()
@@ -35,6 +47,24 @@ public class EnemySpawner : MonoBehaviour
             
             Instantiate(EndPrefab, randomPos, Quaternion.identity);
             endSpawned = true;
+        }
+    }
+    private void SpawnCircleEnemies(int count, float radius)
+    {
+        Vector3 playerPos = player.position;
+
+        for (int i = 0; i < count; i++)
+        {
+            float angle = i * Mathf.PI * 2 / count;
+            Vector3 spawnPos = new Vector3(
+                Mathf.Cos(angle) * radius,
+                Mathf.Sin(angle) * radius,
+                0
+            ) + playerPos;
+
+            Enemy enemy = EnemyPoolManager.Instance.GetRandomEnemy();
+            enemy.transform.position = spawnPos;
+            enemy.gameObject.SetActive(true);
         }
     }
     
